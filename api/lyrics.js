8118e4997db1f0a43c57841eb94225c09dd65420
kronikks-afk <borisenjoyer@gmail.com>
2026-08-27 02:29:51 -0400

    Update lyrics.js

--- api/lyrics.js
+++ api/lyrics.js
@@ -48,7 +48,7 @@ module.exports = async (req, res) => {
 
   try {
     const response = await fetch(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent',
       {
         method: 'POST',
         headers: {
